8b4e3f283d6a6e67cd9029c18f6a2c476d639acf
Enrique Goberna <egobernagarcia@gmail.com>
2025-09-28 12:28:57 +0200

    feat(infra): Terraform bootstrap (S3+DynamoDB+OIDC) + ECS Fargate stack y CI/CD con GitHub Actions

--- infra/env/variables.tf
+++ infra/env/variables.tf
@@ -1,12 +1,50 @@
-variable "aws_region"     { type=string  default="eu-west-1" }
-variable "vpc_cidr"       { type=string  default="10.20.0.0/16" }
-variable "image_tag"      { type=string  default="latest" }
-variable "spring_profile" { type=string  default="default" } # "default" (H2) o "pg"
-variable "task_cpu"       { type=string  default="512" }
-variable "task_memory"    { type=string  default="1024" }
-variable "desired_count"  { type=number  default=1 }
+variable "aws_region" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.20.0.0/16"
+}
+
+variable "image_tag" {
+  type    = string
+  default = "latest"
+}
+
+variable "spring_profile" {
+  type    = string
+  default = "default" # "default" (H2) o "pg"
+}
+
+variable "task_cpu" {
+  type    = string
+  default = "512"
+}
+
+variable "task_memory" {
+  type    = string
+  default = "1024"
+}
+
+variable "desired_count" {
+  type    = number
+  default = 1
+}
 
 # HTTPS / Dominio (opcional)
-variable "enable_https" { type=bool   default=false }
-variable "domain_name"  { type=string default="enriquegoberna.com" }
-variable "subdomain"    { type=string default="order-tracking" } # => order-tracking.enriquegoberna.com
+variable "enable_https" {
+  type    = bool
+  default = false
+}
+
+variable "domain_name" {
+  type    = string
+  default = "enriquegoberna.com"
+}
+
+variable "subdomain" {
+  type    = string
+  default = "order-tracking" # => order-tracking.enriquegoberna.com
+}
